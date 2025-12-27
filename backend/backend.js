21ed546c4e5c93e43cfaa8f2bd94d5dfd571f294
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2025-12-27 14:45:37 +0100

    feat(backend): connect mongo with mongoose

--- backend/backend.js
+++ backend/backend.js
@@ -1,18 +1,26 @@
 // server.js
 
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+// Make the Connection to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Mongo connected"))
+  .catch((err) => console.log("Mongo error:", err.message));
+
+// Route test => health check 
+app.get("/health", (req, res) => {
   res.send("Backend running");
 });
 
+// Launche the server 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
