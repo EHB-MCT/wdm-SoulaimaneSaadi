2239ea0626e661ed267f531fb6948542e1aa2499
Soulaimane Saadi <146191762+SoulaimaneSaadi@users.noreply.github.com>
2026-01-03 21:32:11 +0100

    feat(child): add currentItem field

--- backend/models/Child.js
+++ backend/models/Child.js
@@ -1,11 +1,8 @@
-import mongoose from "mongoose";
-
 const ChildSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
   status: String,
-  isRestricted: Boolean
+  isRestricted: Boolean,
+  currentItem: String
 });
-
-export default mongoose.model("Child", ChildSchema);
